f3c6e75fb367451779f3a754389ae9dfd5e04bd7
Jack <hungjack1222@gmail.com>
2024-01-26 17:04:47 +0800

    deploy

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <the-heading title="RememberME"></the-heading>
+    <the-resources></the-resources>
+</template>
+
+<script>
+import TheHeading from './components/layouts/TheHeading.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
+export default {
+    components: {
+        TheHeading,
+        TheResources,
+    },
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/vue-prj-cmp-01-starting-setup/'
+      : '/'
+  }
